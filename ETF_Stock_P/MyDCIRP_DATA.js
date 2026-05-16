9d5cc9a4bea07ae0faf98f391a4d000d4a6e7113
Mr.Music <jyj0629@ntels.com>
2026-05-17 06:21:54 +0900

    Project ReBuilding

---
+++ ETF_Stock_P/MyDCIRP_DATA.js
@@ -0,0 +1,340 @@
+window.MyDCIRP_DATA = {
+  "DC": [
+    {
+      "name": "ACE AI반도체TOP3+",
+      "code": "469150",
+      "currentPrice": "74660",
+      "currentCompare": "+825",
+      "currentRate": "+1.12",
+      "type": "공격",
+      "buyDate": "2026-04-16",
+      "buyPrice": "64,964",
+      "quantity": "68"
+    },
+    {
+      "name": "ACE KRX금현물",
+      "code": "411060",
+      "currentPrice": "31040",
+      "currentCompare": "-180",
+      "currentRate": "-0.58",
+      "type": "공격",
+      "buyDate": "2026-04-16",
+      "buyPrice": "30,722",
+      "quantity": "145"
+    },
+    {
+      "name": "ACE 미국배당다우존스",
+      "code": "402970",
+      "currentPrice": "15260",
+      "currentCompare": "+40",
+      "currentRate": "+0.26",
+      "buyPrice": "12,292",
+      "quantity": "532",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "ACE 미국빅테크TOP7 Plus",
+      "code": "465580",
+      "currentPrice": "26055",
+      "currentCompare": "+385",
+      "currentRate": "+1.50",
+      "buyPrice": "24,745",
+      "quantity": "249",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "ACE 엔비디아채권혼합",
+      "code": "448540",
+      "currentPrice": "27340",
+      "currentCompare": "+245",
+      "currentRate": "+0.90",
+      "buyPrice": "26,003",
+      "quantity": "428",
+      "type": "안전",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "KODEX 미국반도체",
+      "code": "390390",
+      "currentPrice": "58625",
+      "currentCompare": "+615",
+      "currentRate": "+1.06",
+      "buyPrice": "53,029",
+      "quantity": "186",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "KODEX 삼성전자채권혼합",
+      "code": "448330",
+      "currentPrice": "17580",
+      "currentCompare": "+260",
+      "currentRate": "+1.50",
+      "buyPrice": "16,670",
+      "quantity": "1019",
+      "type": "안전",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "KODEX 헬스케어",
+      "code": "266420",
+      "currentPrice": "20655",
+      "currentCompare": "+585",
+      "currentRate": "+2.91",
+      "buyPrice": "16,753",
+      "quantity": "417",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "PLUS K방산",
+      "code": "449450",
+      "currentPrice": "72700",
+      "currentCompare": "+810",
+      "currentRate": "+1.13",
+      "buyPrice": "78,516",
+      "quantity": "151",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "PLUS 미국달러SOFR금리액티브(합성)",
+      "code": "456200",
+      "currentPrice": "64015",
+      "currentCompare": "-200",
+      "currentRate": "-0.31",
+      "buyPrice": "63,520",
+      "quantity": "84",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "TIGER 200 중공업",
+      "code": "139230",
+      "currentPrice": "18990",
+      "currentCompare": "-490",
+      "currentRate": "-2.52",
+      "buyPrice": "20,062",
+      "quantity": "111",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "TIGER 구리실물",
+      "code": "160580",
+      "currentPrice": "",
+      "currentCompare": "",
+      "currentRate": "",
+      "buyPrice": "15,795",
+      "quantity": "340",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "TIGER 미국테크TOP10 INDXX",
+      "code": "381170",
+      "currentPrice": "34765",
+      "currentCompare": "+705",
+      "currentRate": "+2.07",
+      "buyPrice": "32,239",
+      "quantity": "119",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    },
+    {
+      "name": "TIGER 미국필라델피아반도체나스닥",
+      "code": "381180",
+      "currentPrice": "47180",
+      "currentCompare": "+670",
+      "currentRate": "+1.44",
+      "buyPrice": "40,223",
+      "quantity": "119",
+      "type": "공격",
+      "buyDate": "2026-04-16"
+    }
+  ],
+  "IRP": [
+    {
+      "name": "KODEX 200미국채혼합",
+      "code": "284430",
+      "currentPrice": "23110",
+      "currentCompare": "+115",
+      "currentRate": "+0.50",
+      "buyPrice": "21,567",
+      "quantity": "380",
+      "type": "안전",
+      "buyDate": "2026-04-30"
+    },
+    {
+      "name": "KODEX AI반도체핵심장비",
+      "code": "471990",
+      "currentPrice": "33420",
+      "currentCompare": "+250",
+      "currentRate": "+0.75",
+      "buyPrice": "33,027",
+      "quantity": "259",
+      "type": "공격",
+      "buyDate": "2026-04-30"
+    },
+    {
+      "name": "KODEX TRF3070",
+      "code": "329650",
+      "currentPrice": "14495",
+      "currentCompare": "+20",
+      "currentRate": "+0.14",
+      "buyPrice": "14,520",
+      "quantity": "144",
+      "type": "안전",
+      "buyDate": "2026-04-30"
+    },
+    {
+      "name": "KODEX 미국S&P500에너지(합성)",
+      "code": "218420",
+      "currentPrice": "19830",
+      "currentCompare": "+110",
+      "currentRate": "+0.56",
+      "buyPrice": "19,866",
+      "quantity": "139",
+      "type": "공격",
+      "buyDate": "2026-05-20"
+    },
+    {
+      "name": "KODEX 미국빅테크10(H)",
+      "code": "314250",
+      "currentPrice": "59035",
+      "currentCompare": "+375",
+      "currentRate": "+0.64",
+      "buyPrice": "57,173",
+      "quantity": "120",
+      "type": "공격",
+      "buyDate": "2026-05-06"
+    },
+    {
+      "name": "SOL 미국배당미국채혼합50",
+      "code": "490490",
+      "currentPrice": "11545",
+      "currentCompare": "-5",
+      "currentRate": "-0.04",
+      "buyPrice": "11,522",
+      "quantity": "530",
+      "type": "공격",
+      "buyDate": "2026-04-30"
+    },
+    {
+      "name": "TIGER 글로벌혁신블루칩TOP10",
+      "code": "464930",
+      "currentPrice": "22320",
+      "currentCompare": "+320",
+      "currentRate": "+1.45",
+      "buyPrice": "21,338",
+      "quantity": "152",
+      "type": "공격",
+      "buyDate": "2026-05-06"
+    },
+    {
+      "name": "TIGER 미국필라델피아반도체나스닥",
+      "code": "381180",
+      "currentPrice": "47180",
+      "currentCompare": "+670",
+      "currentRate": "+1.44",
+      "buyPrice": "42,629",
+      "quantity": "11",
+      "type": "공격",
+      "buyDate": "2026-05-06"
+    },
+    {
+      "name": "TIGER 코리아배당다우존스",
+      "code": "0052D0",
+      "currentPrice": "16780",
+      "currentCompare": "+465",
+      "currentRate": "+2.85",
+      "buyPrice": "15,956",
+      "quantity": "21",
+      "type": "공격",
+      "buyDate": "2026-04-13"
+    }
+  ],
+  "연금저축": [
+    {
+      "name": "KODEX 구리선물(H)",
+      "code": "138910",
+      "currentPrice": "10170",
+      "currentCompare": "-130",
+      "currentRate": "-1.26",
+      "buyPrice": "9,365",
+      "quantity": "3",
+      "type": "공격",
+      "buyDate": "2026-04-28"
+    },
+    {
+      "name": "ACE 미국나스닥100",
+      "code": "367380",
+      "currentPrice": "33510",
+      "currentCompare": "+345",
+      "currentRate": "+1.04",
+      "buyPrice": "30,575",
+      "quantity": "58",
+      "type": "공격",
+      "buyDate": "2026-04-26"
+    },
+    {
+      "name": "HANARO Fn조선해운",
+      "code": "441540",
+      "currentPrice": "34475",
+      "currentCompare": "-970",
+      "currentRate": "-2.74",
+      "buyPrice": "35,311",
+      "quantity": "141",
+      "type": "공격",
+      "buyDate": "2026-01-12"
+    },
+    {
+      "name": "ACE AI반도체포커스",
+      "code": "469150",
+      "currentPrice": "74660",
+      "currentCompare": "+825",
+      "currentRate": "+1.12",
+      "buyPrice": "44,401",
+      "quantity": "76",
+      "type": "공격",
+      "buyDate": "2026-01-12"
+    },
+    {
+      "name": "TIGER 미국테크TOP10채권혼합",
+      "code": "472170",
+      "currentPrice": "14725",
+      "currentCompare": "+135",
+      "currentRate": "+0.93",
+      "buyPrice": "13,732",
+      "quantity": "135",
+      "type": "공격",
+      "buyDate": "2026-01-12"
+    }
+  ],
+  "키움증권": [
+    {
+      "name": "PLUS K방산",
+      "code": "449450",
+      "currentPrice": "72700",
+      "currentCompare": "+810",
+      "currentRate": "+1.13",
+      "buyPrice": "80,360",
+      "quantity": "125",
+      "type": "공격",
+      "buyDate": "2026-03-04"
+    },
+    {
+      "name": "HLB테라퓨틱스",
+      "code": "115450",
+      "currentPrice": "14725",
+      "currentCompare": "+135",
+      "currentRate": "+0.93",
+      "buyPrice": "13,766",
+      "quantity": "478",
+      "type": "공격",
+      "buyDate": "2019-01-12"
+    }
+  ]
+};
